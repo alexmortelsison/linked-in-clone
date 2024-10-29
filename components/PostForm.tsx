6bbed7a483400f6a0694ca4954dfe81b968b95a2
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-10-29 12:51:31 +0900

    updated

---
+++ components/PostForm.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Button } from "./ui/button";
+import { ImageIcon } from "lucide-react";
+import { useRef } from "react";
+
+function PostForm() {
+  const ref = useRef<HTMLFormElement>(null);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const { user } = useUser();
+
+  return (
+    <div>
+      <form ref={ref} action="">
+        <div className="flex  items-center space-x-2">
+          <Avatar>
+            <AvatarImage src={user?.imageUrl} />
+            <AvatarFallback>
+              {user?.firstName?.charAt(0)}
+              {user?.lastName?.charAt(0)}
+            </AvatarFallback>
+          </Avatar>
+
+          <input
+            ref={fileInputRef}
+            type="text"
+            name="postInput"
+            placeholder="Start writing a post..."
+            className="flex-1 outline-none rounded-full py-3 px-4 border"
+          />
+
+          <input type="file" name="image" accept="image/*" hidden />
+          <button type="submit" hidden>
+            Post
+          </button>
+        </div>
+
+        {/* Preview conditional check */}
+        <div>
+          <Button>
+            <ImageIcon className="mr-2" size={16} color="currentColor" />
+            Add
+          </Button>
+
+          {/* Add a remove preview button */}
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default PostForm;
